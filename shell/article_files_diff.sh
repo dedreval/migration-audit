ef920eea8d1979f0dda475a3852f57fcb8695fb2
dedreval <dedreval@wiley.com>
2021-02-15 05:22:58 -0500

    leading zero removed in article directory

--- shell/article_files_diff.sh
+++ shell/article_files_diff.sh
@@ -3,18 +3,20 @@ rm /DSS_TMP/AUDIT/diff_files/*
 curl http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/journals.xqy -s >  journals
 
 while read j; do
-  curl "http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/articlefiles.xqy?journal=$j" -s >> /DSS_TMP/AUDIT/diff_files/orig_article_files;
-  echo -e "\n" >> /DSS_TMP/AUDIT/diff_files/orig_article_files;
+  echo $j;
+  curl "http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/articlefiles.xqy?journal=$j" -s >> /DSS_TMP/AUDIT/diff_files/orig_article_files_$j;
+  echo -e "\n" >> /DSS_TMP/AUDIT/diff_files/orig_article_files_$j;
+  sqlplus -S DSS/DSS@AUS-LNDSSDBP-03.WILEY.COM:1593/DSSPRD @article_files.sql $j > /DSS_TMP/AUDIT/diff_files/copy_article_files_$j;
+  pushd /DSS_TMP/AUDIT/diff_files/;
+  cat orig_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'pdf_first_page' | sed  -E 's/^(.*)\t(.*)\t(.+\/.+\/.+\/.+\/[^0]+)(0*)([^0]*.+\/.+)$/\1\t\2\t\3\5/g' | grep -v 'null' | sort -T ../tmp > orig_article_files_sorted_$j;
+  cat copy_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'content_item_id' | grep -v 'undefined' | grep -v 'pdf_first_page' | sed 's/0//g' | sort -T ../tmp > copy_article_files_sorted_$j;
+  diff orig_article_files_sorted_$j copy_article_files_sorted_$j | grep '<' > article_files_in_orig_not_in_copy_$j;
+  diff orig_article_files_sorted_$j copy_article_files_sorted_$j | grep '>' > article_files_in_copy_not_in_orig_$j;
+  cat article_files_in_orig_not_in_copy_$j >> missing;
+  cat article_files_in_copy_not_in_orig_$j >> extra;
+  popd;
 done <journals
 
-sqlplus -S DSS/DSS@AUS-LNDSSDBP-03.WILEY.COM:1593/DSSPRD < article_files.sql > /DSS_TMP/AUDIT/diff_files/copy_article_files
 
-pushd /DSS_TMP/AUDIT/diff_files/
 
-cat orig_article_files | grep -v '9999-9999' | grep '\S' | grep -v 'pdf_first_page' | sort -T ../tmp > orig_article_files_sorted
-cat copy_article_files | grep -v '9999-9999' | grep '\S' | grep -v 'undefined' | grep -v 'pdf_first_page' | sort -T ../tmp > copy_article_files_sorted
 
-comm -2 orig_article_files_sorted copy_article_files_sorted > article_files_in_orig_not_in_copy 
-comm -1 orig_article_files_sorted copy_article_files_sorted > article_files_in_copy_not_in_orig
-
-popd
--- shell/issue_files_diff.sh
+++ shell/issue_files_diff.sh
@@ -1,17 +1,26 @@
 curl http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/journals.xqy -s >  journals
 
+rm /DSS_TMP/AUDIT/iss_files/*
+
+echo $DIR
+
 while read j; do
-  curl "http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/issuefiles.xqy?journal=$j" -s >> /DSS_TMP/files_diff/orig_issue_files;
-  echo -e "\n" >> /DSS_TMP/files_diff/orig_issue_files;
+  curl "http://car-lndsmlep-02.wiley.com:9002/com/wiley/dss/issuefiles.xqy?journal=$j" -s >> /DSS_TMP/AUDIT/iss_files/orig_issue_files;
+  echo -e "\n" >> /DSS_TMP/AUDIT/iss_files/orig_issue_files;
 done <journals
 
-sqlplus -S DSS/DSS@AUS-LNDSSDBP-03.WILEY.COM:1593/DSSPRD < issue_files.sql > /DSS_TMP/files_diff/copy_issue_files
+sqlplus -S DSS/DSS@AUS-LNDSSDBP-03.WILEY.COM:1593/DSSPRD < issue_files.sql > /DSS_TMP/AUDIT/iss_files/copy_issue_files
 
-cd /DSS_TMP/files_diff
+pushd /DSS_TMP/AUDIT/iss_files
 
-cat orig_issue_files | grep -v '9999-9999' | sort > orig_issue_files_sorted
+cat orig_issue_files | grep -v '9999-9999' | grep '\S' | grep -v 'null' | sort > orig_issue_files_sorted
 
-cat copy_issue_files | grep -v '9999-9999' | sort > copy_issue_files_sorted
+cat copy_issue_files | grep -v '9999-9999' | grep '\S' | sort > copy_issue_files_sorted
 
-diff orig_issue_files_sorted copy_issue_files_sorted | grep '<' | awk '{print $2}' > issue_files_in_orig_not_in_copy 
-diff orig_issue_files_sorted copy_issue_files_sorted | grep '>' | awk '{print $2}' > issue_files_in_copy_not_in_orig 
+diff orig_issue_files_sorted copy_issue_files_sorted | grep '<' > issue_files_in_orig_not_in_copy 
+
+diff orig_issue_files_sorted copy_issue_files_sorted | grep '>' > issue_files_in_copy_not_in_orig 
+
+cat issue_files_in_orig_not_in_copy
+
+popd
