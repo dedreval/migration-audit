850f3e48873aed7931bcd31f414364190f187f2f
nzhivogl <nzhivogl@wiley.com>
2021-03-23 15:49:27 +0300

    suppl pattern corrected

--- shell/article_files_diff.sh
+++ shell/article_files_diff.sh
@@ -1,4 +1,5 @@
 BASE_DIR=/DSS_TMP/AUDIT/art_files
+TMP_DIR=/DSS_TMP/AUDIT/tmp
 echo $BASE_DIR
 
 pushd () {
@@ -8,6 +9,9 @@ pushd () {
 popd () {
     command popd "$@" > /dev/null
 }
+
+[[ ! -d $TMP_DIR ]] && mkdir -p $TMP_DIR
+rm -rf $TMP_DIR/*
 
 [[ ! -d $BASE_DIR ]] && mkdir -p $BASE_DIR
 rm -rf $BASE_DIR/*
@@ -28,8 +32,8 @@ EXIT
 EOF
  
   pushd $BASE_DIR/
-  cat orig_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'pdf_first_page' | sed  -E 's/^([^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^0-9]+)(0*)([^0]*.+\/.+)$/\1\3/g' | grep -v 'null' | grep -v '.suppl' | sort -T ../tmp > orig_article_files_sorted_$j
-  cat copy_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'content_item_id' | grep -v 'undefined' | grep -v 'pdf_first_page' |  sed  -E 's/^([^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^0-9]+)(0*)([^0]*.+\/.+)$/\1\3/g' | sort -T ../tmp > copy_article_files_sorted_$j
+  cat orig_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'pdf_first_page' | sed  -E 's/^([^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^0-9]+)(0*)([^0]*.+\/.+)$/\1\3/g' | grep -v 'null' | grep -v '\.suppl' | sort -T $TMP_DIR > orig_article_files_sorted_$j
+  cat copy_article_files_$j | grep -v '9999-9999' | grep '\S' | grep -v 'content_item_id' | grep -v 'undefined' | grep -v 'pdf_first_page' |  sed  -E 's/^([^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^\/]+\/[^0-9]+)(0*)([^0]*.+\/.+)$/\1\3/g' | sort -T $TMP_DIR > copy_article_files_sorted_$j
   diff orig_article_files_sorted_$j copy_article_files_sorted_$j | grep '< ' > article_files_in_orig_not_in_copy_$j
   diff orig_article_files_sorted_$j copy_article_files_sorted_$j | grep '> ' > article_files_in_copy_not_in_orig_$j
   cat article_files_in_orig_not_in_copy_$j >> missing
